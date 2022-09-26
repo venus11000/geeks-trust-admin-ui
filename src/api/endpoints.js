09dcbbb6dcd844107e2c9a81e377687f4a91dc82
Venugopal <venugopala@arzooo.com>
2022-09-27 00:55:44 +0530

    Updates

--- src/api/endpoints.js
+++
@@ -1,4 +0,0 @@
-export const endpoints = {
-  users:
-    "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json",
-};
--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-export const get = async (url) => {
-  const response = await fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      return res;
-    });
-  return response;
-};
